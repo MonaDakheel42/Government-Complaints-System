8e3492aa15bdf9f7cd70916aa40c6cae16b3424f
MonaDakheel42 <monadakheel8@gmail.com>
2025-11-14 14:23:30 +0300

    Initial commit

---
+++ app/Http/Controllers/GovernmentsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GovernmentsController extends Controller
+{
+    //
+}
---
+++ app/Models/Complaint_histories.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Complaint_histories extends Model
+{
+    /** @use HasFactory<\Database\Factories\ComplaintHistoriesFactory> */
+    use HasFactory;
+}
